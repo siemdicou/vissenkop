33e50936df06499850a348eaf03627066dad3efb
siem dicou <siemdicou@siems-MacBook-Pro.local>
2016-11-28 13:26:14 +0100

    some style changes

--- model/getArticles.php
+++ model/getArticles.php
@@ -1,13 +1,24 @@
 
+
 <?php 
+
 	require '../includes/config.php';
 	$result = $mysqli->query( "SELECT * FROM articles");
 	while($row = $result->fetch_assoc()) {
         echo ' 
+        <div class="col-lg-4 col-md-4 col-sm-4 gallery">
+				<a href="work.html">
+					<img src="img/'. $row["img"] . '" class="img-responsive">
+				 	<h3 class="content_title">'.$row["title"].'</h3>
+				 	<div class="content_article"><p>'.$row["content"].'</p></div>
+				 	<div class="content_author">'.$row["author"].'</div>
+				 	<div class="content_author">'.$row["date"].'</div>
 
-                <h3 class="berichttitle">'.$row["title"].'</h3>'.$row["content"]."<Br>".
-                $row["img"]."<Br>".
-                $row["author"]."<Br>".
-                $row["date"]."<Br>".
-                $row["id"];}
+
+				</a>
+		</div>
+
+
+
+';}
  ?>
