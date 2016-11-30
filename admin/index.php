41153cefe154127d08fe815fa7afbfc7e4bab6ef
siem dicou <siemdicou@siems-MacBook-Pro.local>
2016-11-30 10:26:21 +0100

    admin login

---
+++ admin/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script src="../sweetalert-master/dist/sweetalert.min.js"></script>
+<link rel="stylesheet" type="text/css" href="../sweetalert-master/dist/sweetalert.css">
+<link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
+<style type="text/css">
+
+</style>
+	<title>test</title>
+</head>
+<body>
+
+
+
+<script type="text/javascript">
+
+swal({
+    title: "Log In to Continue",
+    html: true,
+    allowEscapeKey:false,
+    text: "<form action='login_model.php' method='POST'>Username: <input type='text' name='Username'><br><br>Password: <input type='password' name='password'> <Br><Br><input type='submit' name='' class='btn btn-info' value='inloggen'></form> "
+});
+
+</script>
+</body>
+</html>
+
