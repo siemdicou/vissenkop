1134390a0a96bcac6af501087ffcd5eabbe7c43c
siemdicou <siemdicou@hotmail.com>
2017-01-30 11:21:37 +0100

    product

---
+++ index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="assets/ico/favicon.html">
+
+
+    <title>Vissenkoppen</title>
+
+    <!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/fonts/FISHERMAN.ttf" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <!-- Static navbar -->
+    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.html"><img src="assets/img/logo.png" alt="logo"></a>
+        </div>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="index.html"><img src="assets/img/contact2.png"/></a></li>
+          </ul>
+      </div>
+    </div>
+
+<div class="headercont">
+	<div id="headerwrap">
+	    <div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-lg-offset-3">
+					<img src="assets/img/header2.png" alt="header1">
+				</div>
+			</div><!--/row -->
+	    </div> <!-- /container -->
+	</div><!--/headerwrap -->
+</div>
+
+<section id="works" class="works">
+<img class="graatsvg" src="assets/img/graat.svg" alt="graat">
+	<div class="container">
+        <h1>Vissenkoppen</h1>
+		<div class="row centered mt mb">
+			
+				<div id="template_content"></div>
+				
+		</div><!--/row -->
+	</div><!--/container -->
+	
+
+</section>
+	<div id="social">
+		<div class="container">
+			<div class="row centered">
+				
+				<div class="col-lg-4">
+					<a href="#"><i class="fa fa-facebook"></i></a>
+				</div>
+				<div class="col-lg-4">
+					<a href="#"><i class="fa fa-twitter"></i></a>
+				</div>
+				<div class="col-lg-4">
+					<a href="#"><i class="fa fa-instagram"></i></a>
+				</div>
+				
+			
+			</div><!--/row -->
+		</div><!--/container -->
+	</div><!--/social -->
+
+	<div id="footerwrap">
+		<div class="container">
+			<div class="row centered">
+				<div class="col-lg-4">
+					<p><b>Kijken doe je met je ogen</b></p>
+				</div>
+			
+				<div class="col-lg-4">
+					<p>Zien doen je in ijmuiden</p>
+				</div>
+				<div class="col-lg-4">
+					<p>g.jaapstok@wiewwav.nl</p>
+				</div>
+			</div>
+		</div>
+	</div><!--/footerwrap -->
+	
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script type="text/javascript">
+window.addEventListener('load', function() {
+     
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("template_content").innerHTML = xhttp.responseText;
+            }
+        };
+        xhttp.open("GET", "model/getArticles.php", true);
+        xhttp.send();
+    // var clicked = document.getElementById("").click
+}, false);
+
+
+</script>
+    </body>
+</html>
