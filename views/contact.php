958fa47e3eda58270b592244c81bcac971cdb642
siemdicou <siemdicou@hotmail.com>
2017-02-01 12:39:15 +0100

    lightbox

--- views/contact.php
+++ views/contact.php
@@ -18,7 +18,60 @@
 	</div><!--/headerwrap -->
 </div>
 
-<section id="works" class="works"></section>
+<section id="works" class="works">
+      
+  <div class="container">
+      <div class="row">
+
+     <div class="col-md-6 ">
+                <div class="contact">
+      <h1>Waar</h1>
+         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d38898.17811428513!2d4.547446399880326!3d52.458561979697535!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5f1cb8c50af3d%3A0xdd4e26411860392e!2sIJmuiden!5e0!3m2!1snl!2snl!4v1485939727871" width="400" height="480" frameborder="0" style="border:0" allowfullscreen></iframe>
+          </div>
+          </div>
+   
+  <div class="col-md-6">
+       <div class="contact">
+      
+          <h2>Mijn gegevens</h2>
+                <div class="contacttext">
+           <p>Audrey Stam</p>
+           <p>06-12345678</p>
+       
+      <p>audreystam@hotmail.com</p>
+           <p> Langstraat 12</p>
+           <p>Ijmuiden</p>
+           </div>
+          </div>
+          </div>
+          </div>
+    </div>
+    
+    
+
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      </section>
 
 	
 
